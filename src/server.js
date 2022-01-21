26702b2364e08d3b8b80dad7f78086f52e632db0
khapu <daikhanh9260@gmail.com>
2022-01-22 02:23:44 +0700

    update

---
+++ src/server.js
@@ -0,0 +1,99 @@
+const khapuDebug = require('./services/debuggerService');
+
+const value = {
+    test1: {
+        test11: {
+            test111: {
+                test1111: {
+                    test11112: "test",
+                    tes11: {
+                        testttt1239489: {
+                            testknahanha: {
+                                kahanhhhh: {
+                                    llllaaZ: "123323"
+                                },
+                                tes11: {
+                                    testttt1239489: {
+                                        testknahanha: {
+                                            kahanhhhh: {
+                                                llllaaZ: "123323"
+                                            },
+                                            tes11: {
+                                                testttt1239489: {
+                                                    testknahanha: {
+                                                        kahanhhhh: {
+                                                            llllaaZ: "123323",
+                                                            tes11: {
+                                                                testttt1239489: {
+                                                                    testknahanha: {
+                                                                        kahanhhhh: {
+                                                                            llllaaZ: "123323",
+                                                                            tes11: {
+                                                                                testttt1239489: {
+                                                                                    testknahanha: {
+                                                                                        kahanhhhh: {
+                                                                                            llllaaZ: "123323",
+                                                                                            llllaaZ1: "123323",
+                                                                                            llllaaZ2: "123323",
+                                                                                            llllaaZ3: "123323",
+                                                                                            llllaaZ4: "123323",
+                                                                                            llllaaZ5: "123323",
+                                                                                            llllaaZ6: "123323",
+                                                                                            llllaaZ7: "123323",
+                                                                                            llllaaZ8: "123323",
+                                                                                            llllaaZ9: "123323",
+                                                                                            llllaaZ0: "123323",
+                                                                                            llllaaZa: "123323",
+                                                                                            llllaaZs: "123323",
+                                                                                            llllaaZd: "123323",
+                                                                                            llllaaZx: "123323",
+                                                                                            llllaaZz: "123323",
+                                                                                            llllaaZx: "123323",
+                                                                                            llllaaZv: "123323",
+                                                                                            llllaaZb: "123323",
+                                                                                            llllaaZg: "123323",
+                                                                                            llllaaZd: "123323",
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                test1112: 123,
+                tesst111: [1,2,3]
+            },
+            tea: ass
+        },
+        test12: "test3"
+    },
+    test2: "test4"
+};
+
+function ass () {
+    const test = 123
+    const aa = 124;
+    for(const i = 1; i < 0; i++) {
+        console.log(1);
+    }
+    return 1 + 1;
+}
+
+const ac = 123;
+// khapuDebug.show(value, {debug: true, showFunction: true});
+khapuDebug.showMultiple([value, ass, ac]);
+
+const test = 1 + 1;
+console.log(test);
