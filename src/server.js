9186f43ec8a47590f5f2496ec6340a9b6dc58c1b
khapu <daikhanh9260@gmail.com>
2022-01-22 16:37:50 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,26 +1,5 @@
-# khapu-debugger
+const khapu = require('./services/debuggerService');
 
----
-# Introduction
-
-The package has been developed to support you debug in an easier way. With khapu-debugger, you are going to forget your trouble when logging variable, because:
- * Provides an intuitive interface, vivid colors
- * Convenient customization (showing level if the variable is a object or array)
- * Sepecifily, you can read function in the variable
- 
----
-
-# Install
-```
-
-npm i khapu-debugger
-
-```
-
-# Usage
-
-```js
-const khapu = require('khapu-debugger')
 
 const value = {
     abc: {
@@ -41,40 +20,21 @@ const value = {
     xyz: "xyz"
 }
 
-const secondValue = "secondValue"
-
 function demo () {
     const num = 123
     const str = "123";
     return numb + str;
 }
 
-khapu.show(value)
+const secondValue = "secondValue"
 
-```
-
-You can show with mutiple params
-
-```js
-khapu.showMutiple([value, secondeValue])
-```
-
-Set options to use
-```js
 const options = {
     stop: true, //boolean //used if you want to stop program when debugging
     showFunction: true, //boolean //used if you want to view function in the debugging variable
     showLevel: null //number //used to set level which you want to show, if its value is null, all level of the variable
 }
 
-
-khapu.show(value, options)
+// khapu.show(value, options)
 //or
-khapu.showMultiple([value, secondValue], options)
-
-```
-
----
-# Demo results
-
+khapu.showMultiple([value, secondValue])
 
